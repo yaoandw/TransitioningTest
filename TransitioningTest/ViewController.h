42cedaee7da5ccbc73660da15a67492e1691802d
yaoandw <yaoandw@163.com>
2015-03-03 15:35:10 +0800

    Initial Commit

---
+++ TransitioningTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitioningTest
+//
+//  Created by yaoandw on 15/3/3.
+//  Copyright (c) 2015年 yycon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TransitioningTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TransitioningTest
+//
+//  Created by yaoandw on 15/3/3.
+//  Copyright (c) 2015年 yycon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
